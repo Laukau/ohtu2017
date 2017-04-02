5fe9f9cca61c2de4451fbdb839affb6edea5f804
Laura <laura.kaurijoki@helsinki.fi>
2017-04-02 22:33:47 +0300

    Viikon 3 tehtävä 2

--- gradle-projekti/src/main/java/Main.java
+++ gradle-projekti/src/main/java/Main.java
@@ -3,11 +3,17 @@ import ohtu.Multiplier;
 
 public class Main {
     public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-        Multiplier kolme = new Multiplier(3);
+        Scanner scanner = new Scanner(System.in);
+        Multiplier kolme = new Multiplier(2);
         System.out.println("anna luku ");
         int luku = scanner.nextInt();
-
+        /* rikkoo checkstyle-säännön
+        for( int i=0; i<1; i++ ) {
+            for( int j=0; i<j; j++ ) {
+              System.out.println("virhe");
+            }
+          }
+        */
         System.out.println("luku kertaa kolme on "+kolme.multipliedBy(luku) );
     }
 }
